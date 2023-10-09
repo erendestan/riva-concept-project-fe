dcd7a41d7ed288012176ad5f129b4fe02dcf065b
Eren Destan <e.destan@student.fontys.nl>
2023-10-09 12:29:51 +0200

    Navigation bar is not hard coded anymore and fixes done on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,62 +1,134 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+// import { Navbar, Nav, Container } from "react-bootstrap";
 import "../styling/navbar-style.css";
 import RivaLogo from "../assets/images/logo.png";
+import Container from "react-bootstrap/Container";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import Carrousel from "./Carrousel";
+import { ButtonGroup } from "react-bootstrap";
+
+const links = [
+  {
+    id: 1,
+    path: "/",
+    text: "Home",
+  },
+  {
+    id: 2,
+    path: "/#aboutus",
+    text: "About Us",
+  },
+  {
+    id: 3,
+    path: "/#photogallery",
+    text: "Photo Gallery",
+  },
+  {
+    id: 4,
+    path: "https://www.instagram.com/rivaconcept.bg/",
+    icon: <FontAwesomeIcon icon={faInstagram} style={{ color: "#ffffff" }} />,
+    eventkey: 1,
+  },
+  {
+    id: 5,
+    path: "https://www.facebook.com/RivaHaskovo",
+    icon: <FontAwesomeIcon icon={faFacebook} style={{ color: "#ffffff" }} />,
+    eventkey: 2,
+  },
+  {
+    id: 6,
+    path: "#contactus",
+    text: "Contact Us",
+    icon: <FontAwesomeIcon icon={faPhone} style={{ color: "#ffffff" }} />,
+    eventkey: 3,
+  },
+  {
+    id: 7,
+    path: "/signup",
+    text: "Sign Up",
+  },
+  {
+    id: 8,
+    path: "/signin",
+    text: "Sign In",
+  },
+];
 
 const NavbarRiva = () => {
   return (
-    <Router>
-      <Navbar collapseOnSelect expand="lg" className="fixed-top navbar-scroll">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={RivaLogo} alt="Riva Logo" width="50" height="50" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link className="text-white" href="#home">
-                Home
-              </Nav.Link>
-              <Nav.Link className="text-white" href="#aboutus">
-                About Us
-              </Nav.Link>
-              <Nav.Link className="text-white" href="#photogallery">
-                Photo Gallery
-              </Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="https://www.instagram.com/rivaconcept.bg/">
-                <FontAwesomeIcon
-                  icon={faInstagram}
-                  style={{ color: "#ffffff" }}
-                />
-              </Nav.Link>
-              <Nav.Link
-                eventKey={2}
-                href="https://www.facebook.com/RivaHaskovo"
-              >
-                <FontAwesomeIcon
-                  icon={faFacebook}
-                  style={{ color: "#ffffff" }}
-                />
-              </Nav.Link>
-              <Nav.Link eventKey={3} href="#phonenumber">
-                <FontAwesomeIcon icon={faPhone} style={{ color: "#ffffff" }} />
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      <Routes>
-        <Route path="#home/photogallery" element={<Carrousel />} />
-      </Routes>
-    </Router>
+    <Navbar collapseOnSelect expand="lg" className="fixed-top navbar-scroll">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={RivaLogo} alt="Riva Logo" width="50" height="50" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {/* <Nav className="me-auto"> */}
+          {links.map((link) => {
+            if (link.id < 4) {
+              return (
+                <Nav className="me-auto" key={link.id}>
+                  {
+                    <Nav.Link className="text-white" href={link.path}>
+                      {link.text}
+                    </Nav.Link>
+                  }
+                </Nav>
+              );
+            }
+            if (link.id > 3 && link.id < 7) {
+              if (link.id !== 6) {
+                return (
+                  <Nav key={link.id}>
+                    <Nav.Link
+                      eventKey={link.eventkey}
+                      className="hover-animation"
+                      href={link.path}
+                    >
+                      {link.icon}
+                    </Nav.Link>
+                  </Nav>
+                );
+              } else {
+                return (
+                  <Nav key={link.id}>
+                    <Nav.Link
+                      eventKey={link.eventkey}
+                      className="hover-animation text-white"
+                      href={link.path}
+                    >
+                      {link.icon}
+                      <span> </span>
+                      {link.text}
+                    </Nav.Link>
+                  </Nav>
+                );
+              }
+            } else {
+              return (
+                <Nav key={link.id}>
+                  <Nav.Link href={link.path}>
+                    <Button variant="primary">{link.text}</Button>
+                  </Nav.Link>
+                </Nav>
+              );
+            }
+          })}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
